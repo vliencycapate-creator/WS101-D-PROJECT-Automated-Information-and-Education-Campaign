9aed40bc18d75060db7779ffcab37f8572b50805
Gem <jhemricporlage14@gmail.com>
2025-11-27 14:20:12 +0800

    Added new config to handle and or reroute  image request

---
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/configs/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.backend_gradle.iec_server.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path currentDir = Paths.get("").toAbsolutePath();
+        Path baseFolder = currentDir.resolve("IEC-PROJECT/Backend/iec-server/uploads");
+        if (!Files.exists(baseFolder)) {
+            try {
+                Files.createDirectories(baseFolder);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + baseFolder + "/");
+    }
+}
+
