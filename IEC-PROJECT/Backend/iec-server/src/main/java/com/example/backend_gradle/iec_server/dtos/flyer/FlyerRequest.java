698830aa6b6ffdb27f462b3050b89259a3ce7a12
Gem <jhemricporlage14@gmail.com>
2025-11-27 14:11:37 +0800

    Added new properties and anotations for validation

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/flyer/FlyerRequest.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/flyer/FlyerRequest.java
@@ -1,4 +1,31 @@
 package com.example.backend_gradle.iec_server.dtos.flyer;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FlyerRequest {
+    @NotBlank
+    @Size(min = 5, message = "Title must have at least 5 characters")
+    private String title;
+
+    @NotBlank
+    @Size(min = 5, message = "Description must have at least 5 characters")
+    private String description;
+
+    @NotBlank
+    @Size(min = 3, message = "Category must have at least 3 characters")
+    private String category;
+
+    @NotNull
+    private int createdBy;
+
+    private MultipartFile[] images;
 }
