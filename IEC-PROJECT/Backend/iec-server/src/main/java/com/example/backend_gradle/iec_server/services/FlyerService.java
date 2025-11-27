ff6c9e560e7b231936d29cd1b7fb074e42bbfc6e
Gem <jhemricporlage14@gmail.com>
2025-11-27 14:18:11 +0800

    Added new changes to service layer

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/services/FlyerService.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/services/FlyerService.java
@@ -1,35 +1,86 @@
 package com.example.backend_gradle.iec_server.services;
 
-import com.example.backend_gradle.iec_server.dtos.flyer.FlyerDto;
+import com.example.backend_gradle.iec_server.dtos.flyer.FlyerRequest;
 import com.example.backend_gradle.iec_server.entities.Flyer;
+import com.example.backend_gradle.iec_server.exceptions.ApiException;
+import com.example.backend_gradle.iec_server.helpers.handlers.ImageUploader;
+import com.example.backend_gradle.iec_server.helpers.responses.ResponseBuilder;
 import com.example.backend_gradle.iec_server.mappers.FlyerMapper;
+import com.example.backend_gradle.iec_server.mappers.ImageMapper;
 import com.example.backend_gradle.iec_server.repositories.FlyerJpaRepository;
-import lombok.AllArgsConstructor;
+import com.example.backend_gradle.iec_server.repositories.ImageJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.io.IOException;
 
 @Service
-@AllArgsConstructor
 public class FlyerService {
     private final FlyerJpaRepository flyerRepo;
-    private UserService userService;
-    private FlyerMapper flyerMapper;
+    private final ImageJpaRepository imageRepo;
+    private final UserService userService;
+    private final FlyerMapper flyerMapper;
+    private final ImageMapper imageMapper;
+    private final ImageUploader imageUploader;
 
-    public List<FlyerDto> getAllFlyers() {
+    @Autowired
+    public FlyerService(
+            ImageUploader imageUploader,
+            ImageMapper imageMapper,
+            FlyerMapper flyerMapper,
+            UserService userService,
+            ImageJpaRepository imageRepo,
+            FlyerJpaRepository flyerRepo
+    ) {
+        this.imageUploader = imageUploader;
+        this.imageMapper = imageMapper;
+        this.flyerMapper = flyerMapper;
+        this.userService = userService;
+        this.imageRepo = imageRepo;
+        this.flyerRepo = flyerRepo;
+    }
+
+    public ResponseEntity<?> getAllFlyers() {
         var flyers = this.flyerRepo.findAll();
-        return flyers.stream()
+        var flyersDto = flyers.stream()
                 .map(flyer -> {
                     var flyerDto = flyerMapper.toDto(flyer);
                     flyerDto.setCreatedBy(userService.getUserById(flyer.getCreatedBy()).getUsername());
                     return flyerDto;
-                }).toList();
+                })
+                .toList();
+        return ResponseBuilder.success("Flyers fetch successful", flyersDto);
     }
 
-    public FlyerDto getFlyerById(int id) {
-        var flyer = this.flyerRepo.findById(id).orElseThrow(() -> new RuntimeException("gem is love"));
+    public ResponseEntity<?> getFlyerById(int id) {
+        var flyer = this.flyerRepo.findById(id).orElseThrow(() -> new ApiException("No Flyer found with id: " + id, HttpStatus.NOT_FOUND));
         var flyerDto = flyerMapper.toDto(flyer);
         flyerDto.setCreatedBy(userService.getUserById(flyer.getCreatedBy()).getUsername());
-        return flyerDto;
+        return ResponseBuilder.success("Flyer fetch successful", flyer);
     }
+
+    public ResponseEntity<?> postFlyer(FlyerRequest flyerRequest) throws IOException {
+        Flyer flyer = flyerMapper.toEntity(flyerRequest);
+        var savedFlyer = this.flyerRepo.save(flyer);
+        var images = this.imageUploader.uploadImage(savedFlyer.getId(), flyerRequest.getImages())
+                .stream()
+                .map(uploadedImage -> {
+                    var image = this.imageMapper.toEntity(uploadedImage);
+                    image.setFlyer(savedFlyer);
+                    return image;
+                })
+                .toList();
+        this.imageRepo.saveAll(images);
+        return ResponseBuilder.created("Flyer created successful");
+    }
+
+    public ResponseEntity<?> deleteFlyerById(int id) {
+        var flyer = this.flyerRepo.findById(id).orElseThrow(() -> new ApiException("No Flyer found with id: " + id, HttpStatus.NOT_FOUND));
+        this.flyerRepo.deleteById(flyer.getId());
+        return ResponseBuilder.success("Flyer deleted successful", null);
+    }
+
+
 }
