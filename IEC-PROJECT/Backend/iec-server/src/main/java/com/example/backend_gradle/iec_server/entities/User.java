36afc4196271ca168e07e423565b8109a261dff1
Gem <jhemricporlage14@gmail.com>
2025-11-27 11:44:30 +0800

    Added new anotations as well as modified new properties to match database constraints

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/entities/User.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/entities/User.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -29,5 +31,5 @@ public class User {
     private String role;
 
     @Column(name = "created_at")
-    private String createdAt;
+    private LocalDateTime createdAt;
 }
