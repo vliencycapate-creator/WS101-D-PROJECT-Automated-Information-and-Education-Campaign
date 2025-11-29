04dbd680d57c3b8c74c7333bf395ff8d4ffb5516
Serrona, Ralph_233691 <itsmeralphrolsergserrona@gmail.com>
2025-11-29 19:36:24 +0800

    Delete IEC-PROJECT/Database/images_table.sql

--- IEC-PROJECT/Database/images_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE  images(
-  image_id INT NOT NULL AUTO_INCREMENT,
-  flyer_id INT NOT NULL,
-  image_name VARCHAR(100) NOT NULL,
-  image_path VARCHAR(255) NOT NULL,
-  PRIMARY KEY (image_id),  
-  FOREIGN KEY (flyer_id) REFERENCES flyers (flyer_id) ON DELETE CASCADE);
