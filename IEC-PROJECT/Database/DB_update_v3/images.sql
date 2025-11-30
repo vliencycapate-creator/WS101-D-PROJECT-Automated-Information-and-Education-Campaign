a079ca9386d9e1f307c80181c4778c6edb63ff4e
NEOCODER <itsmeralphrolsergserrona@gmail.com>
2025-11-30 16:29:37 +0800

    New update for IED Database

---
+++ IEC-PROJECT/Database/DB_update_v3/images.sql
@@ -0,0 +1,7 @@
+CREATE TABLE images (
+	image_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    flyer_id BIGINT NOT NULL UNIQUE,
+    image_name VARCHAR(100) NOT NULL,
+    image_path VARCHAR(255) NOT NULL,
+    FOREIGN KEY (flyer_id) REFERENCES flyers(flyer_id) ON DELETE CASCADE
+);
