13095cf017715419a4c00965c551db1c1556ebb3
Gem <jhemricporlage14@gmail.com>
2025-12-03 21:07:58 +0800

    Update UserDto role

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/user/UserDto.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/user/UserDto.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto{
-    private int id;
+public class UserDto {
+    private long id;
     private String username;
     private String email;
-    private String role;
-    private String createdAt;
+    private List<String> roles;
 }
