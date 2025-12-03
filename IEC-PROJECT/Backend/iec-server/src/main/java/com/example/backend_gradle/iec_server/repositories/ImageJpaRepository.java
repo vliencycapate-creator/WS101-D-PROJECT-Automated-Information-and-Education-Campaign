df47e5efe756238e23fcb0dc981dd6b4ec69ef81
Gem <jhemricporlage14@gmail.com>
2025-12-03 21:35:28 +0800

    Update ImageJpaRepository added a new query

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/repositories/ImageJpaRepository.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/repositories/ImageJpaRepository.java
@@ -3,11 +3,14 @@ package com.example.backend_gradle.iec_server.repositories;
 import com.example.backend_gradle.iec_server.entities.Image;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ImageJpaRepository extends JpaRepository<@NonNull Image, @NonNull Long> {
-    List<Image> findAllById(Long id);
+    @Query(value = "SELECT * FROM images where flyer_id = :id", nativeQuery = true)
+    List<Image> findAllById(@Param("id") Long id);
 }
