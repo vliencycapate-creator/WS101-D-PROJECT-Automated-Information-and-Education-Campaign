5ed1e9ee60a7c092e00acbb7be48e33f0aca3185
Gem <jhemricporlage14@gmail.com>
2025-12-03 21:14:44 +0800

    ...

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/utils/ImageHelper.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/utils/ImageHelper.java
@@ -1,5 +1,4 @@
-package com.example.backend_gradle.iec_server.helpers.utils;
-
+package com.example.backend_gradle.iec_server.utils;
 
 import com.example.backend_gradle.iec_server.dtos.other.UploadedImage;
 import com.example.backend_gradle.iec_server.entities.Image;
@@ -8,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,36 +15,41 @@ import java.util.List;
 
 @Component
 public class ImageHelper {
+
     private static final Path baseFolder = Paths.get("").toAbsolutePath().resolve("IEC-PROJECT/Backend/iec-server/uploads");
 
-    public List<UploadedImage> uploadImage(long id, MultipartFile[] files) throws IOException {
+    public List<UploadedImage> uploadImage(long id, MultipartFile[] files) {
         List<UploadedImage> uploadedImages = new ArrayList<>();
-        if (!Files.exists(baseFolder)) {
-            Files.createDirectories(baseFolder);
-        }
-
-        String folderName = "flyer_" + id;
-        Path folderPath = baseFolder.resolve(folderName);
-        if (!Files.exists(folderPath)) {
-            Files.createDirectories(folderPath);
-        }
-
-        for (MultipartFile file : files) {
-            if (file.isEmpty() || file.getContentType() == null || !file.getContentType().startsWith("image/")) {
-                continue;
+        try {
+            if (!Files.exists(baseFolder)) {
+                Files.createDirectories(baseFolder);
             }
 
-            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-            Path filePath = folderPath.resolve(fileName);
-            file.transferTo(filePath.toFile());
+            String folderName = "flyer_" + id;
+            Path folderPath = baseFolder.resolve(folderName);
+            if (!Files.exists(folderPath)) {
+                Files.createDirectories(folderPath);
+            }
 
-            String urlPath = "/images/" + folderName + "/" + fileName;
-            uploadedImages.add(new UploadedImage(fileName, urlPath));
+            for (MultipartFile file : files) {
+                if (file.isEmpty() || file.getContentType() == null || !file.getContentType().startsWith("image/")) {
+                    continue;
+                }
+
+                String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+                Path filePath = folderPath.resolve(fileName);
+                file.transferTo(filePath.toFile());
+
+                String urlPath = "/images/" + folderName + "/" + fileName;
+                uploadedImages.add(new UploadedImage(fileName, urlPath));
+            }
+            return uploadedImages;
+        } catch (Exception e) {
+            throw new ApiException(e.getMessage(), HttpStatus.CONFLICT);
         }
-        return uploadedImages;
     }
 
-    public void removeImages(List<Image> images) throws IOException {
+    public void removeImages(List<Image> images) {
         images.forEach(image -> {
             try {
                 String pathName = image.getImagePath().substring(8);
