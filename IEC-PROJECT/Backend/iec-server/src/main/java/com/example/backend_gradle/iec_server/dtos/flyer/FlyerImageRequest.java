4634f32734c98720104162ff8e59ed8deed7c1f8
Gem <jhemricporlage14@gmail.com>
2025-12-03 21:27:44 +0800

    Added FlyerImageRequest

---
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/flyer/FlyerImageRequest.java
@@ -0,0 +1,21 @@
+package com.example.backend_gradle.iec_server.dtos.flyer;
+
+import com.example.backend_gradle.iec_server.utils.validation_interface.ImageOnly;
+import com.example.backend_gradle.iec_server.utils.validation_interface.ImageSize;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlyerImageRequest {
+    @ImageOnly(message = "Must have a valid image")
+    @ImageSize(max = 5_000_000, message = "Image size must be below 5mb")
+    private MultipartFile[] images;
+    private List<Long> imagesToDel;
+}
