9213436052481c329a63bd9c899b942711aca502
Gem <jhemricporlage14@gmail.com>
2025-12-03 18:33:07 +0800

    Update UserService added new services and safe transactions

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/services/UserService.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/services/UserService.java
@@ -1,24 +1,67 @@
 package com.example.backend_gradle.iec_server.services;
 
+import com.example.backend_gradle.iec_server.configs.SecurityConfig;
+import com.example.backend_gradle.iec_server.dtos.other.ResponseToken;
+import com.example.backend_gradle.iec_server.dtos.user.LoginRequest;
+import com.example.backend_gradle.iec_server.dtos.user.RegistrationRequest;
 import com.example.backend_gradle.iec_server.dtos.user.UserDto;
+import com.example.backend_gradle.iec_server.entities.User;
+import com.example.backend_gradle.iec_server.exceptions.ApiException;
+import com.example.backend_gradle.iec_server.utils.ResponseBuilder;
+import com.example.backend_gradle.iec_server.utils.ApiAssert;
 import com.example.backend_gradle.iec_server.mappers.UserMapper;
 import com.example.backend_gradle.iec_server.repositories.UserJpaRepository;
+import com.example.backend_gradle.iec_server.security.JwtUtils;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @AllArgsConstructor
 public class UserService {
+
     private final UserJpaRepository userRepo;
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
+    private final SecurityConfig securityConfig;
+    private final JwtUtils jwtUtils;
 
-    public UserDto getUserByEmail(String email) {
-        var user = this.userRepo.findByEmail(email).orElse(null);
-        return userMapper.toDto(user);
+    public ResponseEntity<?> registerUser(RegistrationRequest registrationRequest) {
+        var passEncoder = securityConfig.passwordEncoder();
+        var user = this.userMapper.toEntity(registrationRequest);
+        user.setPassword(passEncoder.encode(user.getPassword()));
+        this.userRepo.save(user);
+        return ResponseBuilder.created("User registered successful");
     }
 
-    public UserDto getUserById(int id) {
-        var user = this.userRepo.findById(id).orElse(null);
-        return userMapper.toDto(user);
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        var user = this.getUserByEmail(loginRequest.getEmail());
+        if(!securityConfig.passwordEncoder().matches(loginRequest.getPassword(), user.getPassword())) throw new ApiException("Invalid email or password", null, HttpStatus.BAD_REQUEST);
+        var token = this.jwtUtils.generateToken(this.userMapper.toDto(user));
+        return ResponseBuilder.success("Login successful",new ResponseToken(token, user.getRole()));
     }
+
+    public User getUserByEmail(String email) {
+        User user = this.userRepo.findByEmail(email).orElse(null);
+        ApiAssert.notFoundIf(user == null, "User not found with email: " + email);
+        return user;
+    }
+
+    public User getUserById(long id) {
+        User user = this.userRepo.findById(id).orElse(null);
+        ApiAssert.notFoundIf(user == null, "User not found with id: " + id);
+        return user;
+    }
+
+    public ResponseEntity<?> getAllUsers(UserDto userDto) {
+        ApiAssert.forbiddenIf(userDto == null || !userDto.getRoles().contains("admin"), "Unauthorized access prohibited!");
+        var users = this.userRepo.findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+        ApiAssert.notFoundIf(users.isEmpty(), "No users found");
+        return ResponseBuilder.success("Users fetched successful", users);
+    }
+
 }
