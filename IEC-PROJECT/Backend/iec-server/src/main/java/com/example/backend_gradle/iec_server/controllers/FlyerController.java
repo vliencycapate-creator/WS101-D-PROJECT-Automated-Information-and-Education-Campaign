e519ae0b7f6d5bf9434b5e5b08eb361be030e56a
Gem <jhemricporlage14@gmail.com>
2025-12-03 17:53:38 +0800

    updated FlyerController.java Added new params and validation

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/controllers/FlyerController.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/controllers/FlyerController.java
@@ -1,39 +1,69 @@
 package com.example.backend_gradle.iec_server.controllers;
 
+import com.example.backend_gradle.iec_server.dtos.flyer.FlyerImageRequest;
 import com.example.backend_gradle.iec_server.dtos.flyer.FlyerRequest;
-import com.example.backend_gradle.iec_server.helpers.validators.ValidationHelper;
+import com.example.backend_gradle.iec_server.dtos.flyer.RequestFlyerStatus;
+import com.example.backend_gradle.iec_server.dtos.user.UserDto;
+import com.example.backend_gradle.iec_server.utils.ValidationHelper;
 import com.example.backend_gradle.iec_server.services.FlyerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/iec-server/api/v1")
+@AllArgsConstructor
 public class FlyerController {
 
     private final FlyerService flyerService;
     private final ValidationHelper validateHelper;
 
-    @Autowired
-    FlyerController (FlyerService flyerService, ValidationHelper validateHelper) {
-        this.flyerService = flyerService;
-        this.validateHelper = validateHelper;
+    @GetMapping("/flyers")
+    public ResponseEntity<?> getFlyers(
+            @AuthenticationPrincipal UserDto user,
+            @RequestParam(required = false, name = "category") String category,
+            @RequestParam(required = false, name = "search") String search) {
+        return this.flyerService.getAllFlyers(user, category, search);
     }
 
-    @GetMapping("/flyers")
-    public ResponseEntity<?> getFlyers() { return this.flyerService.getAllFlyers(); }
-
     @GetMapping("/flyers/{id}")
-    public ResponseEntity<?> getFlyersById(@PathVariable int id) { return this.flyerService.getFlyerById(id); }
+    public ResponseEntity<?> getFlyersById(@PathVariable long id, @AuthenticationPrincipal UserDto user) {
+        return this.flyerService.getFlyerById(id, user);
+    }
 
     @PostMapping("/flyers")
-    public ResponseEntity<?> postFlyer(@ModelAttribute FlyerRequest flyerRequest) throws IOException {
+    public ResponseEntity<?> postFlyer(
+            @ModelAttribute FlyerRequest flyerRequest,
+            @ModelAttribute FlyerImageRequest flyerImageRequest,
+            @AuthenticationPrincipal UserDto user) {
         this.validateHelper.validate(flyerRequest);
-        return this.flyerService.postFlyer(flyerRequest);
+        this.validateHelper.validate(flyerImageRequest);
+        return this.flyerService.postFlyer(flyerRequest, flyerImageRequest, user);
     }
 
     @DeleteMapping("/flyers/{id}")
-    public ResponseEntity<?> deleteFlyerById(@PathVariable int id) { return this.flyerService.deleteFlyerById(id); }
+    public ResponseEntity<?> deleteFlyerById(@PathVariable long id, @AuthenticationPrincipal UserDto user) {
+        return this.flyerService.deleteFlyerById(id, user);
+    }
+
+    @PostMapping("/flyers/{id}")
+    public ResponseEntity<?> UpdateFlyerById(
+            @PathVariable long id,
+            @ModelAttribute FlyerRequest flyerRequest,
+            @ModelAttribute FlyerImageRequest flyerImageRequest,
+            @AuthenticationPrincipal UserDto userDto) {
+        this.validateHelper.validate(flyerRequest);
+        if (flyerImageRequest.getImages() != null) {
+            this.validateHelper.validate(flyerImageRequest);
+        }
+        return this.flyerService.updateFlyerById(id, flyerRequest, flyerImageRequest, userDto);
+    }
+
+    @PutMapping("/flyers/{id}")
+    public ResponseEntity<?> updateFlyerStatusById(@PathVariable long id, @ModelAttribute RequestFlyerStatus requestStatus) {
+        this.validateHelper.validate(requestStatus);
+        return this.flyerService.updateFlyerStatusById(id, requestStatus.getStatus());
+    }
+
 }
