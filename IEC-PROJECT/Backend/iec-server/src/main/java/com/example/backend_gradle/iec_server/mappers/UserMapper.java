0bbfc194377312747d32dbd1c7100c7d91724102
Gem <jhemricporlage14@gmail.com>
2025-12-03 21:26:31 +0800

    ...

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/mappers/UserMapper.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/mappers/UserMapper.java
@@ -1,11 +1,19 @@
 package com.example.backend_gradle.iec_server.mappers;
 
+import com.example.backend_gradle.iec_server.dtos.user.RegistrationRequest;
 import com.example.backend_gradle.iec_server.dtos.user.UserDto;
 import com.example.backend_gradle.iec_server.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "roles",source = "role")
     UserDto toDto(User user);
-    User toEntity(UserDto userDto);
+    User toEntity(RegistrationRequest registrationRequest);
+    default List<String> mapRole(String role) {
+        return role != null ? List.of(role) : List.of();
+    }
 }
