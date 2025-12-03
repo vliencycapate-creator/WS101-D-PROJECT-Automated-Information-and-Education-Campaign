8ef6c641c21f43a36dc1df2dff3446a7e9207694
Gem <jhemricporlage14@gmail.com>
2025-12-03 19:11:57 +0800

    Added LoginRequest properties

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/user/LoginRequest.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/user/LoginRequest.java
@@ -1,4 +1,21 @@
 package com.example.backend_gradle.iec_server.dtos.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
+    @NotBlank
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "password must have at least 6 characters")
+    private String password;
 }
