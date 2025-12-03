b2d5317ae3b5e046f1871f3084915a86e15755dc
Gem <jhemricporlage14@gmail.com>
2025-12-03 21:15:07 +0800

    Update FlyerJpaRepository Added new queries

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/repositories/FlyerJpaRepository.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/repositories/FlyerJpaRepository.java
@@ -3,18 +3,72 @@ package com.example.backend_gradle.iec_server.repositories;
 import com.example.backend_gradle.iec_server.entities.Flyer;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface FlyerJpaRepository extends JpaRepository<@NonNull Flyer, @NonNull Integer> {
-    @Override
-    @NonNull
-    List<Flyer> findAll();
+public interface FlyerJpaRepository extends JpaRepository<@NonNull Flyer, @NonNull Long> {
 
     @Override
     @NonNull
-    Optional<Flyer> findById(Integer id);
+    Optional<Flyer> findById(Long id);
+
+    @Query(value = """
+    SELECT f.* FROM flyers f
+    JOIN users u ON u.user_id = f.created_by
+    WHERE 1 = 1
+    AND (:category IS NULL OR f.category = :category)
+    AND (
+        :search IS NULL OR
+    	(
+    	    (f.title REGEXP CONCAT(
+    	    '(^|[^a-zA-Z0-9])',
+    	    REPLACE(:search ,' ','([^a-zA-Z0-9]|$)|(^|[^a-zA-Z0-9])'),
+    	    '([^a-zA-Z0-9]|$)')
+    	    OR
+        u.username REGEXP CONCAT(
+            '(^|[^a-zA-Z0-9])',
+            REPLACE(:search ,' ','([^a-zA-Z0-9]|$)|(^|[^a-zA-Z0-9])'),
+            '([^a-zA-Z0-9]|$)')
+            )
+        )
+    );
+    """, nativeQuery = true)
+    List<Flyer> findAll(@Param("category") String category, @Param("search") String search);
+
+    @Query(value = """
+    SELECT f.* FROM flyers f
+    JOIN users u ON u.user_id = f.created_by
+    JOIN flyer_records fr ON fr.flyer_id = f.flyer_id
+    WHERE fr.status = 'approved'
+    AND (:category IS NULL OR f.category = :category)
+    AND (
+        :search IS NULL OR
+    	(
+    	    (f.title REGEXP CONCAT(
+    	    '(^|[^a-zA-Z0-9])',
+    	    REPLACE(:search ,' ','([^a-zA-Z0-9]|$)|(^|[^a-zA-Z0-9])'),
+    	    '([^a-zA-Z0-9]|$)')
+    	    OR
+        u.username REGEXP CONCAT(
+            '(^|[^a-zA-Z0-9])',
+            REPLACE(:search ,' ','([^a-zA-Z0-9]|$)|(^|[^a-zA-Z0-9])'),
+            '([^a-zA-Z0-9]|$)')
+            )
+        )
+    );
+    """, nativeQuery = true)
+    List<Flyer> findAllApproved(@Param("category") String category, @Param("search") String search);
+
+    @Query(value = """
+            SELECT f.* FROM flyers f
+            JOIN flyer_records fr on fr.flyer_record_id = f.flyer_record_id
+            WHERE fr.status = 'approved' AND f.flyer_id = :id
+            """, nativeQuery = true)
+    Optional<Flyer> findApprovedById(@Param("id") Long id);
+
 }
