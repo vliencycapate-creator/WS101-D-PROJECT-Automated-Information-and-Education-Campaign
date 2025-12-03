4e6d575944f427d055573099dd7713bd3c37b397
Gem <jhemricporlage14@gmail.com>
2025-12-03 18:03:40 +0800

    Added UserController services

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/controllers/UserController.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/controllers/UserController.java
@@ -1,4 +1,41 @@
 package com.example.backend_gradle.iec_server.controllers;
 
+import com.example.backend_gradle.iec_server.dtos.user.LoginRequest;
+import com.example.backend_gradle.iec_server.dtos.user.RegistrationRequest;
+import com.example.backend_gradle.iec_server.dtos.user.UserDto;
+import com.example.backend_gradle.iec_server.utils.ValidationHelper;
+import com.example.backend_gradle.iec_server.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/iec-server/api/v1")
 public class UserController {
+
+    private final UserService userService;
+    private final ValidationHelper validationHelper;
+
+    @Autowired
+    public UserController(ValidationHelper validationHelper, UserService userService) {
+        this.validationHelper = validationHelper;
+        this.userService = userService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<?> registerUser(@ModelAttribute RegistrationRequest registrationRequest) {
+        this.validationHelper.validate(registrationRequest);
+        return this.userService.registerUser(registrationRequest);
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> loginUser(@ModelAttribute LoginRequest loginRequest) {
+        this.validationHelper.validate(loginRequest);
+        return this.userService.loginUser(loginRequest);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers(@AuthenticationPrincipal UserDto user) { return this.userService.getAllUsers(user); }
+
 }
