69c9bda0eb0357bdbbfaf594e89f0871befb77ad
Gem <jhemricporlage14@gmail.com>
2025-12-04 11:24:40 +0800

    Flyer sample images

---
+++ IEC-PROJECT/Backend/iec-server/src/test/java/com/example/backend_gradle/iec_server/main.java
@@ -0,0 +1,22 @@
+import com.example.backend_gradle.iec_server.dtos.user.UserDto;
+import com.example.backend_gradle.iec_server.entities.Flyer;
+import com.example.backend_gradle.iec_server.security.JwtUtils;
+import com.example.backend_gradle.iec_server.services.UserService;
+import org.apache.catalina.TrackedWebResource;
+
+public static void main(String[] args) {
+//    JwtUtils jwt = new JwtUtils();
+//
+//    String token = jwt.generateToken(new UserDto(1L, "gem", "gem@email.com", List.of("USER")));
+//    System.out.println("Generated Token:");
+//    System.out.println(token);
+//
+//    UserDto user = jwt.verifyToken(token);
+//
+//    System.out.println("\nDecoded Payload:");
+//    System.out.println("ID: " + user.getId());
+//    System.out.println("Username: " + user.getUsername());
+//    System.out.println("Email: " + user.getEmail());
+//    System.out.println("Roles: " + user.getRoles());
+
+}
