934ba6021fc1f12f1cb4c675592ec699ec91fc09
NEOCODER <itsmeralphrolsergserrona@gmail.com>
2025-12-08 16:58:22 +0800

    update flyer record query

--- IEC-PROJECT/Database/iec-database/flyer_records.sql
+++ IEC-PROJECT/Database/iec-database/flyer_records.sql
@@ -1,5 +1,5 @@
 CREATE TABLE flyer_records (
-	flyer_record_id BIGINT NOT NULL PRIMARY KEY,
+	flyer_record_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     flyer_id BIGINT,
     email VARCHAR(100) NOT NULL,
     status ENUM('pending', 'approved', 'declined', 'deleted') DEFAULT 'pending',
