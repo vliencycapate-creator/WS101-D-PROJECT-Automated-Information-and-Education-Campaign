4a97bfb7ded4b4258079b2ee6114e99419e8c2bf
Gem <jhemricporlage14@gmail.com>
2025-12-14 16:07:32 +0800

    Remove roles to registration and update changes to email annotation to have more robust validation

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/user/RegistrationRequest.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/dtos/user/RegistrationRequest.java
@@ -3,7 +3,6 @@ package com.example.backend_gradle.iec_server.dtos.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,14 +15,11 @@ public class RegistrationRequest {
     private String username;
 
     @NotBlank
-    @Email(message = "Invalid email format")
+    @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
     private String email;
 
     @NotBlank
-    @Size(min = 6, message = "password must have at least 6 characters")
+    @Size(min = 8, message = "password must have at least 8 characters")
     private String password;
 
-    @NotBlank
-    @Pattern(regexp = "admin|faculty", message = "Role is invalid")
-    private String role;
 }
