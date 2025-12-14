5076179eb5ab0c8101cb5a315e39a3a352cadc6d
Gem <jhemricporlage14@gmail.com>
2025-12-14 16:03:08 +0800

    Update changes to match php backend output

--- IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/exceptions/GlobalExceptionHandler.java
+++ IEC-PROJECT/Backend/iec-server/src/main/java/com/example/backend_gradle/iec_server/exceptions/GlobalExceptionHandler.java
@@ -15,13 +15,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<@NonNull  Map<String, Object>> handleApiException(ApiException ex) {
         Map<String, Object> body = new HashMap<>();
+        body.put("success", false);
         body.put("status", ex.getStatus().value());
-        body.put("error", ex.getStatus().getReasonPhrase());
         body.put("message", ex.getMessage());
         body.put("timestamp", new java.util.Date());
 
-        if (ex.getData() != null) {
-            body.put("data", ex.getData());
+        if (ex.getErrors() != null) {
+            body.put("errors", ex.getErrors());
         }
 
         return new ResponseEntity<>(body, ex.getStatus());
@@ -30,6 +30,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<@NonNull Map<String, Object>> handleOtherExceptions(Exception ex) {
         Map<String, Object> body = new HashMap<>();
+        body.put("success", false);
         body.put("status", 500);
         body.put("error", "Internal Server Error");
         body.put("message", ex.getMessage());
